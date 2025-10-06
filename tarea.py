6dbe8e7c9b1e0e27100de69f7893b8d341119113
Orlaine-R <orlainerodri@gmail.com>
2025-10-06 11:45:41 -0400

    Add files via upload

---
+++ tarea.py
@@ -0,0 +1,39 @@
+nombre_usuario = "Orlaine Rodriguez"
+edad = 18
+pi = 3.14
+es_estidiante = True
+
+print(f"mi nombre es {nombre_usuario} y tengo {edad} años")
+
+
+print("SUMA:")
+N1 = int(input("primer numero:"))
+N2 = int(input("segundo numero:"))
+
+suma = N1 + N2
+
+print(suma)
+
+print("RESTA:")
+n1 = int(input("primer numero:"))
+n2 = int(input("primer numero:"))
+
+resta = n1 - n2
+
+print(resta)
+
+print("MULTIPLICACION:")
+U1 = int(input("primer numero:"))
+U2 = int(input("primer numero:"))
+
+multiplicacion = U1 * U2
+
+print(multiplicacion)
+
+print("DIVICION:")
+u1 = int(input("primer numero:"))
+u2 = int(input("primer numero:"))
+
+divicion = u1 // u2
+
+print(divicion)
